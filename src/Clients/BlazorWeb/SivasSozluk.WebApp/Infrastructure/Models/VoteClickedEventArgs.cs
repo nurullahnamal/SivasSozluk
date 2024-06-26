3450ee7cc5befdddfe93b3a2f7302a5ce45df4a3
nurullah namal <77736724+nurullahnamal@users.noreply.github.com>
2024-06-26 14:54:46 +0300

    15. update

---
+++ src/Clients/BlazorWeb/SivasSozluk.WebApp/Infrastructure/Models/VoteClickedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿namespace SivasSozluk.WebApp.Infrastructure.Models
+{
+    public class VoteClickedEventArgs : EventArgs
+    {
+        public Guid EntryId { get; set; }
+
+        public bool IsUpVoteClicked { get; set; }
+
+        public bool UpVoteDeleted { get; set; }
+
+
+        public bool IsDownVoteClicked { get; set; }
+        public bool DownVoteDeleted { get; set; }
+    }
+}
